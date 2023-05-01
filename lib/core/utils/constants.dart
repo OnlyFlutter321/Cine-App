86dbf0b14f56efa769f42638217a2b365f0ce351
rahemanali <onlygithub321@gmail.com>
2023-05-01 19:46:47 +0530

    change

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static const String baseUrl = 'https://api.themoviedb.org/3';
+
+  //Colors
+
+  static const Color kMSBlack = Color(0xFF000000);
+  static const Color kMSWhite = Color(0xFFFFFFFF);
+  static const Color kMSGreen = Color(0xCBA0FA44);
+  static const Color kMSOrange = Color(0xCDFFB74D);
+  static const Color kMSRed = Color(0xCBEF5350);
+
+  static const Color kThemeColor = Color(0xFFE83678);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'core/utils/constants.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
   runApp(const Cine());
 }
 
@@ -9,13 +18,35 @@ class Cine extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    MaterialColor msThemeColor =
+        MaterialColor(Constants.kThemeColor.value, const <int, Color>{
+      50: Constants.kThemeColor,
+      100: Constants.kThemeColor,
+      200: Constants.kThemeColor,
+      300: Constants.kThemeColor,
+      400: Constants.kThemeColor,
+      500: Constants.kThemeColor,
+      600: Constants.kThemeColor,
+      700: Constants.kThemeColor,
+      800: Constants.kThemeColor,
+      900: Constants.kThemeColor,
+    });
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Constants.kThemeColor,
+        primarySwatch: msThemeColor,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+      ),
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(),
+        backgroundColor: Constants.kMSWhite,
+        body: const Center(
           child: Text(
             'Lets learn together i want to contribute\nin movie app for bloc',
             style: TextStyle(
-              color: Colors.red,
+              color: Colors.black,
             ),
           ),
         ),
